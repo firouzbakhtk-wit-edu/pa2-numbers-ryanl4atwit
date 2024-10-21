4540c58984fa8cb0da8633d68d213938e446556a
lryan <lryan@10.220.91.50>
2024-10-21 09:37:02 -0400

    Part B passed all of the JUnit tests, Part A only failed one

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,52 @@
 package edu.wit.cs.comp1000;
-
+import java.util.Scanner;
 // TODO: document this class
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		
+		Scanner input = new Scanner(System.in);
+		
+		int posSum = 0;
+		int posCount = 0;
+		int negSum = 0;
+		int negCount = 0;
+		int total = 0;
+		
+		System.out.print("Enter five whole numbers: ");
+		for (int i = 0; i < 5; i++) {
+			
+			int num = input.nextInt();
+			total += num;
+			if (num>0) {
+				posSum += num;
+				posCount ++;
+			}else {
+				negSum += num;
+				negCount ++;
+			}
+		}
+		
+		double posAvg = (double) posSum / posCount;
+		double negAvg = (double) negSum / negCount;
+		double totalAvg = (double) total / 5;
+		
+		if (posCount == 0) {
+			posAvg = 0;
+		}else if (negCount ==0) {
+			negAvg = 0;
+		}
+		
+		System.out.printf("The sum of the %d positive numbers: %d%n", posCount, posSum);
+		System.out.printf("The sum of the %d non-positive numbers: %d%n", negCount, negSum);
+		System.out.printf("The sum of the 5 numbers: %d%n", total);
+		System.out.printf("The average of the %d positive numbers: %.2f%n", posCount, posAvg);
+		System.out.printf("The average of the %d non-positive numbers: %.2f%n", negCount, negAvg);
+		System.out.printf("The average of the 5 numbers: %.2f%n", totalAvg);
+		
+		input.close();
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,33 @@
 package edu.wit.cs.comp1000;
-
+import java.util.Scanner;
 // TODO: document this class
 public class PA2b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a b c: ");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		double discriminant = (b*b) - (4 * a * c);
+		
+		if (discriminant < 0) {
+			System.out.println("Roots: imaginary");
+			System.exit(0);
+		}
+		
+		double root1 = (-b + Math.sqrt(discriminant))/(2*a);
+		if (discriminant == 0) {
+			System.out.printf("Root: %.2f%n", root1);
+		}
+		
+		double root2 = (-b - Math.sqrt(discriminant))/(2*a);
+		if (discriminant > 0) {
+			System.out.printf("Roots: %.2f, %.2f%n", root2, root1);
+		}
 	}
 
 }
